7b9166cf4d219ebf08af10fee9f49f9b3df4a0d2
karma <captainav0608@gmail.com>
2025-06-17 19:59:10 +0530

    modify setup files
    Signed-off-by: karma <captainav0608@gmail.com>

--- models/foodopiaDB.js
+++ models/foodopiaDB.js
@@ -1,11 +1,12 @@
 const mysql = require("mysql2");
+require('dotenv').config();
 
 const db = mysql.createConnection({
     host:"localhost",
     user:"root",
     password: process.env.MYSQL_PASSWORD,
     multipleStatements:true,
-    database: "foodopia_nodeJS",
+    database: process.env.DB_NAME,
 }) 
 
 db.connect((err)=>{
