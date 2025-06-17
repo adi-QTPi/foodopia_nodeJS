2ce2e356fc7f3542732af48f7eb592f94b0606b0
karma <captainav0608@gmail.com>
2025-06-17 19:23:34 +0530

    save necessary files
    Signed-off-by: karma <captainav0608@gmail.com>

--- controllers/api_admin.js
+++ controllers/api_admin.js
@@ -3,6 +3,7 @@ const {db} = require("../models/foodopiaDB");
 async function handle_post_orders_by_date(req, res){
     const asked_date_for_orders = req.body.date;
     req.session.date_to_fetch_order_for = asked_date_for_orders;
+    
     req.session.to_admin_page = {
         ...req.session.to_admin_page,
         page_date:asked_date_for_orders,
