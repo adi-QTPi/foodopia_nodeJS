67e133bf686250abf0331ed3cef8a0a247e1b113
karma <captainav0608@gmail.com>
2025-06-20 22:01:46 +0530

    modify .env.example
    Signed-off-by: karma <captainav0608@gmail.com>

--- models/foodopiaDB.js
+++ models/foodopiaDB.js
@@ -3,7 +3,9 @@ require('dotenv').config();
 
 const db = mysql.createConnection({
     host:"localhost",
-    user:"root",
+    // user:"root",
+    user:process.env.MYSQL_USERNAME,
+    port: process.env.MYSQL_PORT,
     password: process.env.MYSQL_PASSWORD,
     multipleStatements:true,
     database: "foodopia",
