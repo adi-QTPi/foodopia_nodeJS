28ae400365975ee43de91f53cb6f411249889483
karma <captainav0608@gmail.com>
2025-06-26 18:36:42 +0530

    modify req body analyzer middleware to give specific page-wise errors
    Signed-off-by: karma <captainav0608@gmail.com>

--- middleware/general.js
+++ middleware/general.js
@@ -5,9 +5,37 @@ const check_for_entries_in_array = (array_of_required_fields) => {
     return async (req, res, next) => {
         for (const field of array_of_required_fields){
             if(!req.body[field]){
-                return res.status(400).json({
-                    "msg": "please enter all the fields"
-                })
+                let redirect_to = "";
+
+                if(req.originalUrl === "/api/admin/create_new"){
+                    req.session.to_signup_page = {
+                        error:"Please enter all the required fields properly."
+                    }
+                    redirect_to= "/static/admin/create_new";
+                }
+                else if(req.originalUrl === "/api/cat"){
+                    req.session.to_menu_page = {
+                        error:"Please enter all the required fields properly."
+                    }
+                    redirect_to= "/static/menu";
+                }
+                else if(req.originalUrl === "/api/item"){
+                    req.session.to_menu_page = {
+                        error:"Please enter all the required fields properly."
+                    }
+                    redirect_to= "/static/menu";
+                }
+
+                else{
+                    req.session.to_error_page = {
+                        status:400,
+                        status_meaning:"Bad Request",
+                        error: "Please enter all the required fields properly."
+                    }
+                    redirect_to = "/static/error"
+                }
+                
+                return res.redirect(redirect_to);
             }
         }
         next();
