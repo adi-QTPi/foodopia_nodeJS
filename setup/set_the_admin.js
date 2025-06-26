296bc7f042b1fdd57943fb5222e64b42fa861b86
karma <captainav0608@gmail.com>
2025-06-26 18:04:15 +0530

    modify setup files to make just the admin by default
    Signed-off-by: karma <captainav0608@gmail.com>

--- setup/set_the_admin.js
+++ setup/set_the_admin.js
@@ -17,30 +17,9 @@ async function make_the_admin() {
     if(err){
       console.log(err);
     }
-    console.log("admin is added");
+    console.log("New admin with username `admin` is created.");
+    console.log("Creaate New Accounts with different roles using your admin privilege.");
   })
 }
 
 make_the_admin();
-
-const def_cook = {
-  username : process.env.COOK_USER_NAME,
-  name:process.env.COOK_NAME,
-  password : process.env.COOK_PASSWORD
-}
-
-async function make_a_cook() {
-  const salt_rounds = 10;
-  const hashedPassword2 = await bcrypt.hash(def_cook.password, salt_rounds);
-
-  let sql_query = "INSERT INTO user (user_name,name, pwd_hash, role) VALUES (?, ?, ?, ?)"
-
-  db.query(sql_query,[def_cook.username,def_cook.name, hashedPassword2, 'cook'], (err, result, fields)=>{
-    if(err){
-      console.log(err);
-    }
-    console.log("cook is added");
-  } )
-}
-
-make_a_cook();
