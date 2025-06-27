81ab52d3f3079a8a2651fad5f206347501e7bf86
karma <captainav0608@gmail.com>
2025-06-28 00:00:18 +0530

    ask user for mysql address
    Signed-off-by: karma <captainav0608@gmail.com>

--- setup/add_dummy_db.js
+++ setup/add_dummy_db.js
@@ -7,9 +7,9 @@ const m_host = process.env.MYSQL_HOST;
 const m_port = process.env.MYSQL_PORT;
 const m_password = process.env.MYSQL_PASSWORD;
 
-const shell_command = `mysql -u ${m_username} -h ${m_host} -P ${m_port} -p${m_password}`;
+const shell_command = `${shell} -u ${m_username} -h ${m_host} -P ${m_port} -p${m_password}`;
 
-exec(`${shell_command} < setup/add_dummyDB.sql`,{ shell : shell}, (error, stdout , stderr)=>{
+exec(`${shell_command} < setup/add_dummyDB.sql`, (error, stdout , stderr)=>{
     if(error){
         console.log(error);
         return;
--- setup/create_db.js
+++ setup/create_db.js
@@ -7,7 +7,7 @@ const m_host = process.env.MYSQL_HOST;
 const m_port = process.env.MYSQL_PORT;
 const m_password = process.env.MYSQL_PASSWORD;
 
-const shell_command = `mysql -u ${m_username} -h ${m_host} -P ${m_port} -p${m_password}`;
+const shell_command = `${shell} -u ${m_username} -h ${m_host} -P ${m_port} -p${m_password}`;
 
 exec(`${shell_command} < setup/createDB.sql`,{ shell : shell}, (error, stdout , stderr)=>{
     if(error){
